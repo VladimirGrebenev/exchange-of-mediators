6e67896739a5fd2d6b52e0be48b43c61b5a4f74b
veneberg <grebenev-81@mail.ru>
2023-09-03 13:04:11 +0300

    Rebuild New Template (html, CSS Bootstrape, JS). Make jinja/django html-components on mainpage(index.html) and mainpage of working cabinet (page-dashboard.html).

--- mediators_website/mediators_website/urls.py
+++ mediators_website/mediators_website/urls.py
@@ -5,6 +5,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('dashboard/', TemplateView.as_view(
+        template_name='page-dashboard.html'),
+         name='dashboard'),
     path("users/", include("user.urls", namespace="users")),
     path("signing/", include("signing.urls", namespace="signing")),
 ]
