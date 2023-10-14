41bb698773e36cdf5d7fc7962a208dd2239dab3f
Starunov <35844513@bk.ru>
2023-10-14 22:28:02 +0300

    аdded a form for creating responses to conflicts

--- mediators_website/utils/sample_objects.py
+++
@@ -1,7 +0,0 @@
-from random import sample
-
-
-def sample_queryset(model, k=1):
-    """Выборка k случайных неповторяющихся объектов"""
-    k = min(k, model.objects.count())
-    return sample(list(model.objects.all()), k=k)
