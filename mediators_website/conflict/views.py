ed423f9fe0358db490d3a68d79e41361b5828d8a
Still190494 <Still190494@ya.ru>
2023-10-25 21:20:19 +0300

    fix_workplace

--- mediators_website/conflict/views.py
+++ mediators_website/conflict/views.py
@@ -122,7 +122,6 @@ class ConflictCreateView(CreateView):
         return context
 
 
-
 class UserConflictWorkplacelView(LoginRequiredMixin, DetailView):
     """Получение рабочей страницы конфликта у юзера"""
     model = Conflict
@@ -133,17 +132,6 @@ class UserConflictWorkplacelView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['form'] = RespondentsForm()
         return context
-
-    def post(self, request, *args, **kwargs):
-        """Добавление участников конфликта"""
-        form = RespondentsForm(request.POST)
-        if form.is_valid():
-            conflict = self.get_object()
-            respondents = form.cleaned_data.get('respondents', [])
-            for respondent in respondents:
-                conflict.respondents.add(respondent)
-            return redirect('dashboard:conflict-workplace', pk=conflict.pk)
-        return render(request, 'dashboard/page-dashboard-conflict-workplace.html')
 
 
 class MediatorConflictWorkplacelView(LoginRequiredMixin, DetailView):
